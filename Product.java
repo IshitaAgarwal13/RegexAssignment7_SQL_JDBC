7d2789def01c1880428992d75c0d3585802bc485
Ishita Agarwal <67784472+IshitaAgarwal13@users.noreply.github.com>
2021-08-30 14:18:56 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,28 @@
+package com.java.regex.javaReGex;
+
+public class Product {
+	private int pid;
+	private String pname;
+	private int pprice;
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public int getPprice() {
+		return pprice;
+	}
+	public void setPprice(int pprice) {
+		this.pprice = pprice;
+	}
+	
+
+}
